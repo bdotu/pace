3380101a0d88e99c708aeb4faa5650023309eea8
bdotu <kofi.dotu@gmail.com>
2015-11-10 19:28:47 -0600

    added post request and headers

--- pace_api_agent/app/api_agent.rb
+++ pace_api_agent/app/api_agent.rb
@@ -1,23 +1,28 @@
 require 'sinatra'
-# require 'sinatra/base'
 require 'json'
-# require 'open-uri'
-# require 'nokogiri'
-require './lib/crawler'
+require './pace_api_agent/lib/crawler'
 
 
 before do
-  headers "Content-Type" => "text/html; charset=utf-8"
+  content_type :json
+  # headers "Content-Type" => "text/html; charset=utf-8"
+  headers 'Access-Control-Allow-Origin' => '*',
+          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+          'Access-Control-Allow-Headers' => 'Content-Type'
 end
 
 get '/hi' do
   "Hello World!!!"
 end
 
+#puts listing from the crawl in the browser
 get '/getListings' do
   crawler = CrawlerHelper.new
   crawler.crawl
-  # response = crawler.crawl
-  # response.to_json
-  # "Something"
+end
+
+#same priniciple applies in the case of a post request
+post '/getListings' do
+  crawler = CrawlerHelper.new
+  crawler.crawl
 end
